9dd3eee880adee4ee5d1a25a7e5d84eaa8d009fd
Michael Hall <mhall1473@gmail.com>
2022-02-06 11:54:27 -0500

    First pass at products kata

---
+++ productDiscounts.js
@@ -0,0 +1,29 @@
+const singleProductCost = 299
+const twoProductCost = 598 // 299 * 2
+const setOfThreeCost = 807.3 // 299 * 3 * 0.9
+const setOfFourCost = 956.8 // 299 * 4 * 0.8
+const setOfFiveCost = 1121.25 // 299 * 5 * 0.75
+
+
+module.exports.calculateProductTotal = (products) => {
+    const emptyProductCounts = {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0
+    }
+    const productCounts = products.reduce((accumulator, currentProduct) => {
+        return {
+            ...accumulator,
+            [currentProduct]: ++accumulator[currentProduct]
+        }
+    }, emptyProductCounts)
+    const values = Object.values(productCounts).sort()
+
+    return values[0] * setOfFiveCost
+        + Math.max(values[1] - values[0], 0) * setOfFourCost
+        + Math.max(values[2] - values[1], 0) * setOfThreeCost
+        + Math.max(values[3] - values[2], 0) * twoProductCost
+        + Math.max(values[4] - values[3], 0) * singleProductCost
+}
---
+++ productDiscounts.test.js
@@ -0,0 +1,23 @@
+const calculateProductTotal = require('./productDiscounts').calculateProductTotal
+const expect = require('chai').expect
+
+describe('calculateProductTotal', () => {
+
+    const tests = [
+        { products: [], scenario: 'Empty array returns 0', expected: 0},
+        { products: ['A'], scenario: 'A single product costs 299', expected: 299},
+        { products: ['A', 'A'], scenario: 'Two of the same product costs 299', expected: 598},
+        { products: ['A', 'B'], scenario: 'Two unique products costs 598', expected: 598},
+        { products: ['A', 'B', 'C'], scenario: 'Three unique products are discounted 10%', expected: 807.3},
+        { products: ['A', 'B', 'C', 'D'], scenario: 'Four unique products are discounted 20%', expected: 956.8},
+        { products: ['A', 'B', 'C', 'D', 'E'], scenario: 'Five unique products are discounted 25%', expected: 1121.25},
+        { products: ['A', 'A', 'B', 'C'], scenario: 'You are only discounted for sets of products', expected: 1106.3},
+        { products: ['A', 'A', 'B', 'C', 'A', 'B', 'C', 'D', 'E'], scenario: 'Set of 3, Set of 5, and a Single product', expected: 2227.55}
+    ]
+    
+    tests.forEach(({products, scenario, expected}) => {
+        it(`${scenario}`, () => {
+            expect(calculateProductTotal(products)).eql(expected)
+        })
+    })
+})
